4cc1caf233a75f827b762963c02d35edee885b36
je3f0o <je3f0o@gmail.com>
2020-04-07 05:39:20 +0800

    Added index.html page

---
+++ js/main.js
@@ -0,0 +1,18 @@
+/* -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.
+* File Name   : main.js
+* Created at  : 2020-04-07
+* Updated at  : 2020-04-07
+* Author      : jeefo
+* Purpose     :
+* Description :
+* Reference   :
+.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.*/
+// ignore:start
+"use strict";
+
+/* globals*/
+/* exported*/
+
+// ignore:end
+
+console.log("Hello world");
