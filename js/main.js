87c20267b6b2af5f28dccd6a9ca34b12c01ea916
je3f0o <je3f0o@gmail.com>
2020-04-07 08:21:35 +0800

    Merged with source-code commit - 81f0a870c

--- js/main.js
+++ js/main.js
@@ -15,7 +15,20 @@
 
 // ignore:end
 
+// add some new features.
+function add (a, b) {
+    return a + b;
+}
+
+// Test specs
+function assert (test_name, condition) {
+    console.log(`Test '${ test_name }' - ?`, condition ? "Passed" : "Failed");
+}
+
+assert("add function", add(3, 5) === 8);
+
 window.addEventListener("load", () => {
     // initialize application
     console.log("Hello world");
+    console.log("5 + 3 = " + add(5, 3));
 });
